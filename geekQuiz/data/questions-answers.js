daa65d6bf1370ed0c9ae4f86614fac77b85f109c
Nazar-Ferents <nazarferents594@gmail.com>
2026-07-20 16:06:52 +0300

    create greeting page, page for choosing universe and quiz page

---
+++ geekQuiz/data/questions-answers.js
@@ -0,0 +1,19 @@
+
+
+const questionsHarryPotter = [
+    {
+        questionTitle:'Хто головний персонаж?',
+        answers: ['Герміона','Рон','Гаррі','Снейп'],
+        correct:2
+    },
+    {
+        questionTitle:'Хто вбив маму Гаррі?',
+        answers: ['Герміона','Рон','Гаррі','Волдеморт'],
+        correct:3
+    },
+]
+
+
+
+
+export default questionsHarryPotter
---
+++ geekQuiz/data/universes.js
@@ -0,0 +1,22 @@
+import questionsHarryPotter from "../data/questions-answers.js";
+
+
+export const universes =
+    [
+        {
+            title:'Гаррі Поттер',
+            questions:questionsHarryPotter
+        },
+        {
+            title:'Марвел'
+        },
+        {
+            title:'Гра Престолів'
+        },
+        {
+            title:'Аватар: Останній захисник'
+        },
+        {
+            title:'Пірати Карибського моря'
+        }
+        ]
